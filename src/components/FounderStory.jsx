4ada0b5bf39cf28950ca803cb2957c5cbf81c2c9
prasad <prasad@gmail.com>
2026-02-20 14:57:27 +0530

    updated founder name

--- src/components/FounderStory.jsx
+++ src/components/FounderStory.jsx
@@ -27,7 +27,7 @@ const FounderStory = () => {
                                 industry-ready from day one.
                             </p>
                             <footer className={styles.founderFoot}>
-                                <strong>Nagaprasad</strong>
+                                <strong>Prasad</strong>
                                 <span>Founder, ProjectMinds</span>
                             </footer>
                         </div>
